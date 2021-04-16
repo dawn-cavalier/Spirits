494efe62347ff2435a810f92a63af5b8bceccf79
ttoper <tyler.toper@ufl.edu>
2021-04-16 18:32:53 -0400

    Start up

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <SFML/Graphics.hpp>
+
+int main() {
+    // TODO: Set width and height modifications
+    sf::RenderWindow window(sf::VideoMode(728, 1024), "");
+    return 0;
+}
